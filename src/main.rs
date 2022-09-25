e95d3e4fd631d165400592117be65f30398992e3
凤之兮原 <1577310596@qq.com>
2022-09-25 23:23:44 +0800

    chore: setup

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use std::io;
+fn main() {
+    println!('Guess one number');
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect('Failed to read line');
+}
