16d3bf8d2751e58c99bc6f96ccc0e3350a639a0f
凤之兮原 <1577310596@qq.com>
2022-09-26 20:45:23 +0800

    feat: add match demo

--- src/match/demo.rs
+++ src/match/demo.rs
@@ -8,5 +8,7 @@ fn plus_one(x: Option<i32>) -> Option<i32> {
     match x {
         None => None,
         Some(i) => Some(i + 1),
+        // 相当于 default
+        _ => (),
     }
 }
---
+++ src/match/if_let.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let v = Some(0u8);
+    match v {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+    if let Some(3) = v {
+        println!("three");
+    }
+}
