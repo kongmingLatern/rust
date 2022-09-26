69fedae5bcb671280646dfa3f74cd57917963510
凤之兮原 <1577310596@qq.com>
2022-09-26 20:27:08 +0800

    feat: add String demo

--- src/string/first_world.rs
+++ src/string/first_world.rs
@@ -1,6 +1,9 @@
 fn main() {
     let mut s = String::from("hello World!");
     let word = first_word(&s);
+
+    s.clear();
+    println!("{}", word);
 }
 fn first_word(s: &String) -> usize {
     let bytes = s.as_bytes();
