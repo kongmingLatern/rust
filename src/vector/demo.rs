0ca19669f4b66e928dfcd9c3c342c9d995ff0460
凤之兮原 <1577310596@qq.com>
2022-09-26 21:32:31 +0800

    feat: add vector demo

---
+++ src/vector/demo.rs
@@ -0,0 +1,16 @@
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+fn main() {
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12)
+    ];
+
+    for i in row {
+        println!("{}", i);
+    }
+}
