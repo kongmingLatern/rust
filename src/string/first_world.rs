1fbe0bfdc4a347aba4812063531e28de3798fa72
凤之兮原 <1577310596@qq.com>
2022-09-26 19:49:35 +0800

    feat: add first_world

---
+++ src/string/first_world.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut s = String::from("hello World!");
+    let word = first_word(&s);
+}
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+    s.len()
+}
