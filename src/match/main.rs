3ccee23e98d48777d21a69fcc934f45e9c042a05
凤之兮原 <1577310596@qq.com>
2022-09-26 20:36:15 +0800

    feat: add match demo

--- src/match/main.rs
+++ src/match/main.rs
@@ -1,1 +1,19 @@
-fn main() {}
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+fn main() {
+    let coin = Coin::Penny;
+    let mut count = 0;
+    match coin {
+        Coin::Penny => {
+            println!("Lucky Penny!");
+            count += 1;
+        }
+        Coin::Nickel => println!("Nickel"),
+        Coin::Dime => println!("Dime"),
+        Coin::Quarter => println!("Quarter"),
+    }
+}
