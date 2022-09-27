1313f4460d625621e801713bef14a6500a3f8b67
凤之兮原 <1577310596@qq.com>
2022-09-27 21:37:14 +0800

    feat: add trait

---
+++ src/trait/main.rs
@@ -0,0 +1,12 @@
+use hello_cargo::Summary;
+use hello_cargo::Tweet;
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+}
