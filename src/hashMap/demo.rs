b1ec8f7c5520c050fa9cf1f9a9c5479ac6f8a841
凤之兮原 <1577310596@qq.com>
2022-09-27 20:42:33 +0800

    feat: add HashMap demo

---
+++ src/hashMap/demo.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+}
