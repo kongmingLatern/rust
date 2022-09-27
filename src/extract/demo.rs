5b18dbe9838a2cec7984de942a56b8430ca93cbd
凤之兮原 <1577310596@qq.com>
2022-09-27 21:18:16 +0800

    feat: add Extract demo

---
+++ src/extract/demo.rs
@@ -0,0 +1,30 @@
+// fn largest(list: &[i32]) -> i32 {
+//     let mut largest = list[0];
+
+//     for &item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
+// 泛型
+fn largest<T>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 40, 20, 10, 5];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+}
